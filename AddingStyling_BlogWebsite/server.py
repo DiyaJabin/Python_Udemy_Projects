df3a0bf833b306e2fe15857d49dccd1576eae76c
DiyaJabin <diyasjabin@gmail.com>
2026-03-28 15:41:27 +0530

    Day 60 - Implemented contact form with GET/POST and email sending in Flask

--- AddingStyling_BlogWebsite/server.py
+++ AddingStyling_BlogWebsite/server.py
@@ -1,12 +1,21 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 import requests
+import smtplib
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 app=Flask(__name__)
+
+#-------CONSTANTS--------
 JSON_BIN_ENDPOINT="https://api.npoint.io/691d0e7aa29fd76f3ef5"
+EMAIL=os.getenv("MAIL_ADDRESS")
+PASSWORD=os.getenv("MAIL_PASSWORD")
 
 response=requests.get(JSON_BIN_ENDPOINT)
-print(response.status_code)
 content = response.json()
+
+
 
 @app.route('/')
 def homepage():
@@ -18,10 +27,26 @@ def about():
     return render_template("about.html",heading="About Me",subheading="This is what I do",
                            image="background-image: url('../static/assets/img/about-bg.jpg")
 
-@app.route('/contact')
+@app.route('/contact',methods=['GET','POST'])
 def contact():
-    return render_template("contact.html",heading="Contact Me",subheading="Have questions? I have answers",
+    if request.method =="GET":
+        return render_template("contact.html",heading="Contact Me",subheading="Have questions? I have answers",
                            image="background-image: url('../static/assets/img/contact-bg.jpg")
+    else:
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        message = request.form['message']
+        with smtplib.SMTP('smtp.gmail.com',587) as connection:
+            connection.starttls()
+            connection.login(user=EMAIL,password=PASSWORD)
+            connection.sendmail(
+                from_addr=EMAIL,
+                to_addrs=EMAIL,
+                msg=f"Subject:You got a message!!\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}"
+            )
+        return render_template("form_entry.html")
+
 
 @app.route('/post/<int:num>')
 def post(num):
@@ -37,6 +62,8 @@ def post(num):
         else:
             continue
     return render_template("post.html",post_num=num,posts=content,heading=head,subheading=subhead,image=image_)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
